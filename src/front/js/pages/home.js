552671470c002cc0153e361fa878e68519ef8db4
Allan Roger <105656076+allanrogerfl@users.noreply.github.com>
2024-03-22 21:21:52 +0000

    commit

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,26 +1,121 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+  const { store, actions } = useContext(Context);
 
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
-		</div>
-	);
+  return (
+    <div className="container-fluid justify-content-center mt-5 w-50">
+      <div className="accordion" id="accordionPanelsStayOpen">
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button
+              className="accordion-button"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#panelsStayOpen-collapseOne"
+              aria-expanded="true"
+              aria-controls="panelsStayOpen-collapseOne"
+            >
+              Sign Up
+            </button>
+          </h2>
+          <div
+            id="panelsStayOpen-collapseOne"
+            className="accordion-collapse collapse show"
+          >
+            <div className="accordion-body">
+              You can pick your email, a password and complete registration;
+              your user will be created in the database, then you will be
+              redirected to the login page afterwards. Follow{" "}
+              <a href="https://special-journey-5gq9ppq7vxgw3vg9v-3000.app.github.dev/signup">
+                {" "}
+                this link
+              </a>{" "}
+              to sign up.
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#panelsStayOpen-collapseTwo"
+              aria-expanded="false"
+              aria-controls="panelsStayOpen-collapseTwo"
+            >
+              Login
+            </button>
+          </h2>
+          <div
+            id="panelsStayOpen-collapseTwo"
+            className="accordion-collapse collapse"
+          >
+            <div className="accordion-body">
+              After you fill out your email and password, you will be redirected
+              to the personal infromation dashboard after a successful
+              authentication. If you already have a user, follow{" "}
+              <a href="https://special-journey-5gq9ppq7vxgw3vg9v-3000.app.github.dev/login">
+                this link to login.
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#panelsStayOpen-collapseThree"
+              aria-expanded="false"
+              aria-controls="panelsStayOpen-collapseThree"
+            >
+              Personal Information
+            </button>
+          </h2>
+          <div
+            id="panelsStayOpen-collapseThree"
+            className="accordion-collapse collapse"
+          >
+            <div className="accordion-body">
+              In case your user is confirmed as valid, you will be able to see
+              personal information dashboard. If you are already logged in, you
+              can follow{" "}
+              <a href="https://special-journey-5gq9ppq7vxgw3vg9v-3000.app.github.dev/private">
+                this link
+              </a>{" "}
+              to see your personal information.
+            </div>
+          </div>
+        </div>
+        <div className="accordion-item">
+          <h2 className="accordion-header">
+            <button
+              className="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#panelsStayOpen-collapseFour"
+              aria-expanded="false"
+              aria-controls="panelsStayOpen-collapseFour"
+            >
+              Logout
+            </button>
+          </h2>
+          <div
+            id="panelsStayOpen-collapseFour"
+            className="accordion-collapse collapse"
+          >
+            <div className="accordion-body">
+              At any moment you can press "logout" in the navbar to finish the
+              session, then you will get redirected back to the login path.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
